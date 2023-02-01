e5c9291fbe220dd365ed36ef8d8f6defd12c4c63
RawaanKh <109039734+RawaanKh@users.noreply.github.com>
2023-02-01 09:07:14 +0300

    Update ApiException.java

--- UsersMangementSWE/src/main/java/com/example/usersmangementswe/Exception/ApiException.java
+++ UsersMangementSWE/src/main/java/com/example/usersmangementswe/Exception/ApiException.java
@@ -1,7 +1,7 @@
 package com.example.usersmangementswe.Exception;
 
 public class ApiException extends RuntimeException  {
-    //هنا وراثه من كلاس الاكسبشنز عشان نهندل كل الايرورز
+    
     public ApiException(String message){
         super(message);
     }
