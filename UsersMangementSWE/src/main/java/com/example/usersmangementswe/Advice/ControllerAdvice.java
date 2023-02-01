a9c2a9ffdb60dc14a2d68642197fc40844a32575
RawaanKh <109039734+RawaanKh@users.noreply.github.com>
2023-02-01 09:01:39 +0300

    Update ControllerAdvice.java

--- UsersMangementSWE/src/main/java/com/example/usersmangementswe/Advice/ControllerAdvice.java
+++ UsersMangementSWE/src/main/java/com/example/usersmangementswe/Advice/ControllerAdvice.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-// بعد البريك هنا
+
 @RestControllerAdvice
 public class ControllerAdvice { //#3
     @ExceptionHandler(value = ApiException.class)
@@ -13,8 +13,8 @@ public class ControllerAdvice { //#3
     public ResponseEntity ApiException(ApiException e){
         String message=e.getMessage();
         return ResponseEntity.status(400).body(message);
-              //كل الاحداث اللي ممكن تعطل السيرفر من نوع 400
-        //  نحطها هنا ونفصلها عن200
+            
+       
     }
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
        public ResponseEntity MethodArgumentNotValidException(MethodArgumentNotValidException e){
